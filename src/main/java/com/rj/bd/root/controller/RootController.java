edad7a5ead5e75345b7c2d1f5380dc1f225d84a3
冯天硕 <73507539+bdNianShao@users.noreply.github.com>
2021-04-16 11:04:29 +0800

    完成上传下载功能--2021年4月16日11点04分

--- src/main/java/com/rj/bd/root/controller/RootController.java
+++ src/main/java/com/rj/bd/root/controller/RootController.java
@@ -2,15 +2,27 @@ package com.rj.bd.root.controller;
 
 
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.ContextLoader;
 
 import com.rj.bd.logs.eneity.Logs;
 import com.rj.bd.logs.service.ILogsService;
@@ -18,6 +30,7 @@ import com.rj.bd.root.entity.Root;
 import com.rj.bd.root.service.IRootService;
 import com.rj.bd.tool.DateTool;
 import com.rj.bd.tool.MD5Utils;
+import com.rj.bd.tool.qiniu.PutFile;
 
 
 
@@ -185,6 +198,93 @@ public Map<String, Object> add( Root root)//,int rootid,int staffid
 	map.put("text", "添加成功");
 	return map;
 }	
+
+@RequestMapping("addImg")
+@ResponseBody
+public Map<String, Object> addImg(HttpServletRequest request, HttpServletResponse response) throws Exception
+{
+	Map<String, Object> map = new HashMap<String, Object>();
+	FileItem imageItem = null;
+	String imagePrifix = "";//图片后缀
+	String rootname = null;
+	String rootpassword = null;
+	// 将请求消息实体中每一个项目封装成单独的DiskFileItem(FileItem接口的实现)对象的任务
+	// 将本次请求的request封装成DiskFileItemFactory对象
+	DiskFileItemFactory factory = new DiskFileItemFactory();
+	// 使用ServletFileUpload解析器上传数据，解析结果返回一个List<FileItem>集合，每一个FileItem对应一个Form表单
+	ServletFileUpload upload = new ServletFileUpload(factory);
+	// 设定中文处理
+	upload.setHeaderEncoding("utf-8");
+	List<FileItem> formItemList = null;
+	try 
+	{
+		formItemList = upload.parseRequest(request);
+	}
+	catch (FileUploadException e) 
+	{
+		
+	}
+	
+	if ((formItemList != null) || (formItemList.size() > 0)) 
+	{
+		for (FileItem Item : formItemList) 
+		{
+			if (!Item.isFormField()) {
+				// 如果不是表单（筛选出文件）
+				// 获取文件名字
+				String fileName = Item.getName();
+				System.out.println("上传文件的名字:" + fileName);
+				// 获取后缀
+				String prifix = fileName.substring(fileName.lastIndexOf(".") + 1);
+				// 后缀全部转小写 防止后缀大小写不统一
+				prifix = prifix.toLowerCase();
+				//System.out.println("上传文件的后缀:" + prifix);
+				if (prifix.equals("png") || prifix.equals("jpg") || prifix.equals("bmp") || prifix.equals("tif")
+						|| prifix.equals("gif") || prifix.equals("jpeg")) 
+				{
+					// 仅支持这几种格式的数据
+					imageItem =Item;
+					imagePrifix = prifix;
+					
+				}
+				else
+				{
+					map.put("msc", -1);
+					map.put("text", "添加失败");
+					return map;
+				}
+			}
+			
+			//其它参数
+			else 
+			{
+				if (Item.getFieldName().equals("rootname")) 
+				{
+					 rootname = Item.getString("utf-8");
+				}
+				else if (Item.getFieldName().equals("rootpassword")) 
+				{
+					 rootpassword = Item.getString("utf-8");
+				}
+				
+				
+			}
+			
+			
+		}
+	
+	}
+		String rootavatar = PutFile.Putimgs(imageItem.getInputStream(), imagePrifix);
+		System.out.println(rootavatar);
+		String token =MD5Utils.stringToMD5( rootpassword + rootname);
+		
+		
+	 map.put("msc", 200);
+	 map.put("text", "添加成功");
+	return map;
+	
+}	
+
 
 
 
--- src/main/java/com/rj/bd/tool/qiniu/PutFile.java
+++ src/main/java/com/rj/bd/tool/qiniu/PutFile.java
@@ -1,5 +1,6 @@
-package com.rj.bd.tool;
+package com.rj.bd.tool.qiniu;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
@@ -13,21 +14,20 @@ import com.qiniu.storage.UploadManager;
 import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
 
-import cn.hutool.core.lang.UUID;
-
 /**
-* @desc 
-* 
-* @author TianShuo
-* 
-* @version 2021年4月15日 下午4:59:12
-*/
+ * 
+ * @desc 上传文件测试类
+ * @author
+ * @time
+ */
+@SuppressWarnings("unused")
 public class PutFile {
-	private static String accessKey = "ZpBp-02v6F5Bod37EldpUAtfVHL-manH2t4tIAbk";
-	private static String secretKey = "pEmx2uT-51KTXaQsOvWaI0bg_4moH_wnL8p3jZ38";
-	private static String bucket = "funnystories";
 
+	private static String accessKey = "XIqX87y9n-KDa0S4UzTQZZJ2zKzH1Nc7U2ZFCtwJ";
+	private static String secretKey = "I8BacqHSrSqwGPVHiQglguBwGNTfhI18f3bEXHbN";
+	private static String bucket = "greentrammanager";
 
+	
 	/**
 	 * @desc 上传文件
 	 * @param ImgIO  InputStream
@@ -39,7 +39,7 @@ public class PutFile {
 		UploadManager uploadManager = new UploadManager(cfg);
 
 		// 默认不指定key的情况下，以文件内容的hash值作为文件名
-		String key = UUID.randomUUID().toString() + System.currentTimeMillis() + "." + suffix;
+		String key = SendInfo.GetUUID() + System.currentTimeMillis() + "." + suffix;
 		try {
 			// 设置key认证
 			Auth auth = Auth.create(accessKey, secretKey);
@@ -57,7 +57,7 @@ public class PutFile {
 
 				// System.out.println(putRet.hash);
 				// 返回文件名
-				return "http://file.dreamoforiginal.cn/" + putRet.key;
+				return "http://qralol3sr.bkt.clouddn.com/" + putRet.key;
 			} catch (QiniuException ex) {
 				Response r = ex.response;
 				System.err.println(r.toString());
@@ -74,5 +74,15 @@ public class PutFile {
 		return null;
 	}
 
+	public static void main(String[] args) throws UnsupportedEncodingException {
+
+//		byte[] uploadBytes = "hello qiniu cloud".getBytes("utf-8");
+//		ByteArrayInputStream byteInputStream = new ByteArrayInputStream(uploadBytes);
+//		System.out.println(Putimgs(byteInputStream, "txt"));
+//		System.out.println(accessKey);
+//		System.out.println(secretKey);
+//		System.out.println(bucket);
+
+	}
 
 }
